256048a3e5c852434903cc1d19971b69a94a8a9e
Omer <omeriqbal7@hotmail.com>
2020-04-30 20:38:52 +0500

    css made better

--- src/components/profile/CustomerProfile.jsx
+++ src/components/profile/CustomerProfile.jsx
@@ -32,7 +32,7 @@ function CustomerProfile() {
         })
     },[ID]);
 
-    async function updatename(CustomerID, value){
+    async function updatename(value){
         firebase_integration.auth.currentUser.updateProfile({
             displayName: value,
           }).then(function() {
@@ -44,7 +44,7 @@ function CustomerProfile() {
           });
         
     }
-    async function updateemail(CustomerID, value){
+    async function updateemail(value){
         firebase_integration.auth.currentUser.updateEmail(value).then(function() {
             firebase_integration.database.collection("CustomerDatabase").doc(firebase_integration.auth.currentUser.uid.toString()).update({
                 Email : value
@@ -54,19 +54,19 @@ function CustomerProfile() {
         });
         
     }
-    async function updatenumber(CustomerID, value){
+    async function updatenumber(value){
         firebase_integration.database.collection("CustomerDatabase").doc(firebase_integration.auth.currentUser.uid.toString()).update({
             ContactNo : value
         })
     }
-    async function updatepassword(CustomerID, value){
+    async function updatepassword(value){
         firebase_integration.auth.currentUser.updatePassword(value).then(function() {
             alert("Your password has been updated");
           }).catch(function(error) {
             alert(error.message);
           });
     }
-    async function updategender(CustomerID, value){
+    async function updategender(value){
         firebase_integration.database.collection("CustomerDatabase").doc(firebase_integration.auth.currentUser.uid.toString()).update({
             Gender : value
         })
@@ -82,25 +82,25 @@ function CustomerProfile() {
                     <img id="profilepic" className = "img-fluid" alt="ProfilePicture" />
                     {firebase_integration.getImageURL('profilepic', 'CustomerProfile', '','profilepic.svg')}
                 </div>
-                <div id="r1c2" className ="col-3" style={{paddingLeft: "0px", paddingRight: "0px"}}>
-                    <div class="row">
-                        <div class="col d-flex justify-content-center">
+                <div id="r1c2" className ="col-4" style={{paddingLeft: "0px", paddingRight: "0px"}}>
+                    <div className="row">
+                        <div className="col d-flex justify-content-center">
                             <div id="namebox">
                                 {name}
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="btn-group d-flex" role="group" aria-label="Basic example">
-                                <button type="button" class="btn btn-secondary w-100 buttonbar" style = {{borderLeft: "0px"}}>Profile</button>
-                                <a href = './orderhistory' type="button" class="btn btn-secondary w-100 buttonbar">Order History</a>
-                                <a  href = './feedback' type="button" class="btn btn-secondary w-100 buttonbar">Feedback</a>
+                    <div className="row">
+                        <div className="col">
+                            <div className="btn-group d-flex" role="group" aria-label="Basic example">
+                                <button type="button" className="btn btn-secondary w-100 buttonbar" style = {{borderLeft: "0px"}}>Profile</button>
+                                <a href = './orderhistory' type="button" className="btn btn-secondary w-100 buttonbar">Order History</a>
+                                <a  href = './feedback' type="button" className="btn btn-secondary w-100 buttonbar">Feedback</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div id="r1c3" className="col-5"/>
+                <div id="r1c3" className="col-4"/>
                 <div id="r1c4" className="col-2 d-flex justify-content-center">
                     <img id ="loyaltypoints" className = "img-fluid" alt="LoyaltyPoints" />
                     {firebase_integration.getImageURL('loyaltypoints', 'CustomerProfile', '', 'pointslogo.svg')}
@@ -108,84 +108,84 @@ function CustomerProfile() {
             </div>
             <div className="row">
                 <div id ="r2c1" className ="col-2"/>
-                <div id ="r2c2" className = "col-2 d-flex justify-content-center">
+                <div id ="r2c2" className = "col d-flex">
                     <form>
-                        <div class ="infotitle">
+                        <div className ="infotitle">
                             Name
                             <img id ="editname" className = "img-fluid pencil" alt="pencil" onClick={() => seteditname(true)}/>
                             {firebase_integration.getImageURL('editname', 'CustomerProfile', '', 'pencil.svg')}
                             {editname === false?
-                                <div class="info">{name}</div>:
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="Enter name"aria-label="Recipient's username" aria-describedby="button-addon2" value = {name} onChange={(e) => setname(e.target.value)}/>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditname(false) 
-                                            updatename(ID,name)}}>Save</button>
+                                <div className="info">{name}</div>:
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Enter name"aria-label="Recipient's username" aria-describedby="button-addon2" value = {name} onChange={(e) => setname(e.target.value)}/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditname(false) 
+                                            updatename(name)}}>Save</button>
                                     </div>
                                 </div>
                             }
                         </div>
-                        <div class="infotitle">
+                        <div className="infotitle">
                             Email
                             <img id ="editemail" className = "img-fluid pencil" alt="pencil" onClick={() => seteditemail(true)}/>
                             {firebase_integration.getImageURL('editemail', 'CustomerProfile', '', 'pencil.svg')}
 
                             {editemail === false?
-                                <div class="info">{email}</div>:
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="Enter email" aria-label="Recipient's username" aria-describedby="button-addon2" value = {email}  onChange={(e) => setemail(e.target.value)}/>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditemail(false) 
-                                            updateemail(ID,email)}}>Save</button>
+                                <div className="info">{email}</div>:
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Enter email" aria-label="Recipient's username" aria-describedby="button-addon2" value = {email}  onChange={(e) => setemail(e.target.value)}/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditemail(false) 
+                                            updateemail(email)}}>Save</button>
                                     </div>
                                 </div>
                             }
                         </div>
-                        <div class="infotitle">
+                        <div className="infotitle">
                             Phone Number
                             <img id ="editnumber" className = "img-fluid pencil" alt="pencil" onClick={() => seteditnumber(true)}/>
                             {firebase_integration.getImageURL('editnumber', 'CustomerProfile', '', 'pencil.svg')}
                             {editnumber === false?
-                                <div class="info">{number}</div>:
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="Enter password" aria-label="Recipient's username" aria-describedby="button-addon2" value = {number}  onChange={(e) => setnumber(e.target.value)}/>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditnumber(false) 
-                                            updatenumber(ID, number)}}>Save</button>
+                                <div className="info">{number}</div>:
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Enter password" aria-label="Recipient's username" aria-describedby="button-addon2" value = {number}  onChange={(e) => setnumber(e.target.value)}/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditnumber(false) 
+                                            updatenumber( number)}}>Save</button>
                                     </div>
                                 </div>
                             }
                         </div>
-                        <div class ="infotitle">
+                        <div className ="infotitle">
                             Date of Birth
-                            <div class="info">{dob}</div>
+                            <div className="info">{dob}</div>
                         </div>
-                        <div class="infotitle">
+                        <div className="infotitle">
                             Password
                             <img id ="editpassword" className = "img-fluid pencil" alt="pencil" onClick={() => seteditpassword(true)}/>
                             {firebase_integration.getImageURL('editpassword', 'CustomerProfile', '', 'pencil.svg')}
                             {editpassword === false?
-                                <div class="info">**********</div>:
-                                <div class="input-group mb-3">
-                                    <input type="password" class="form-control" placeholder="Enter New Password" aria-label="Recipient's username" aria-describedby="button-addon2" value = {password}  onChange={(e) => setpassword(e.target.value)}/>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditpassword(false) 
-                                            updatepassword(ID,password)}}>Save</button>
+                                <div className="info">**********</div>:
+                                <div className="input-group mb-3">
+                                    <input type="password" className="form-control" placeholder="Enter New Password" aria-label="Recipient's username" aria-describedby="button-addon2" value = {password}  onChange={(e) => setpassword(e.target.value)}/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditpassword(false) 
+                                            updatepassword(password)}}>Save</button>
                                     </div>
                                 </div>
                             }
                         </div>
-                        <div class="infotitle">
+                        <div className="infotitle">
                             Gender
                             <img id ="editgender" className = "img-fluid pencil" alt="pencil" onClick={() => seteditgender(true)}/>
                             {firebase_integration.getImageURL('editgender', 'CustomerProfile', '', 'pencil.svg')}
                             {editgender === false?
-                                <div class="info">{gender}</div>:
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder={gender} aria-label="Recipient's username" aria-describedby="button-addon2" value = {gender}  onChange={(e) => setgender(e.target.value)}/>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditgender(false) 
-                                            updategender(ID,gender)}}>Save</button>
+                                <div className="info">{gender}</div>:
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder={gender} aria-label="Recipient's username" aria-describedby="button-addon2" value = {gender}  onChange={(e) => setgender(e.target.value)}/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => {seteditgender(false) 
+                                            updategender(gender)}}>Save</button>
                                     </div>
                                 </div>
                             }
