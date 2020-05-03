b69676e984a4eb0a981f472f4827152de6300996
mehreemas <mehreen@DESKTOP-486H0IP.localdomain>
2020-05-04 02:44:05 +0500

    checkout-file-updated-for-backend

--- src/components/order/checkout.jsx
+++ src/components/order/checkout.jsx
@@ -1,12 +1,22 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import './orderstyles.css';
 import Header from './navbar';
 import Footer from '../navigation/footer';
 import DiscountContext from '../context/context';
+import CheckoutContext from '../context/checkoutcontext';
 
 function Checkout() {
 
     const {discount, setDiscount} = useContext(DiscountContext);
+    const [number, setNumber] = useState("");
+    const [address, setAddress] = useState("");
+    const [area, setArea] = useState("");
+    const [floor, setFloor] = useState("");
+    const [deliverytype, setDeliveryType] = useState("");
+    const [paymentMethod] = useState("Cash on Delivery")
+
+    const {orderdetails} = useContext(CheckoutContext);
+    console.log(orderdetails)
     
     return (
         <div class = "menuback">
@@ -14,30 +24,37 @@ function Checkout() {
             <div class = "forms">
                 <div name = "your-details"><h3><strong>Your Details:</strong></h3></div>
                 <form action="/action_page.php">
-                    <input type="text" name ="mobile" placeholder="   Mobile Number" required pattern="[0-9]{10}"/>
-                    <input type="text" name ="address" placeholder="   Street Address"/><br/>
-                    <input type="text" name ="area" placeholder="   Area"/>
-                    <input type="text" name ="floor" placeholder="   Floor/Unit"/>
+                    <input required="required" value={number} onChange={(e) => setNumber(e.target.value)} type="text" name ="mobile" placeholder="   Mobile Number" required pattern="[0-9]{10}"/>
+                    <input required="required" value={address} onChange={(e) => setAddress(e.target.value)} type="text" name ="address" placeholder="   Street Address"/><br/>
+                    <input required="required" value={area} onChange={(e) => setArea(e.target.value)} type="text" name ="area" placeholder="   Area"/>
+                    <input required="required" value={floor} onChange={(e) => setFloor(e.target.value)} type="text" name ="floor" placeholder="   Floor/Unit"/>
                 </form> 
                 <br/> <br/>
                 <div class = "instructions"><h3><strong>Special Instructions:</strong></h3></div>
                 <textarea class = "text-area" placeholder="   Write your text here"></textarea>
                 <br/> <br/>
-                {/* { <div><h3><strong>Order Type:</strong></h3></div> */}
+                <div><h3><strong>Order Type:</strong></h3></div>
                 <div name = "order-type-checkout">
-                    <input type="radio" name = "method"/>
+                    <input value={deliverytype} onChange={(e) => setDeliveryType("Delivery")} type="radio" name = "method"/>
                     <label>Delivery</label>
-                    <input type="radio" name = "method"/>
-                    <label>Cash on Delivery</label>
+                    <input value={deliverytype} onChange={(e) => setDeliveryType("Takeaway")} type="radio" name = "method"/>
+                    <label>Takeaway</label>
                 </div>
                 <div><h3><strong>Select a payment method:</strong></h3></div>
                 <div name = "payment">
                     <input type="radio" checked='checked' name = "method"/>
                     <label>Cash on Delivery</label>                   
                     <div class = "confirm">
-                        <a onClick = {() => {
-                            setDiscount("0%")
-                            console.log(discount)}} href = "/orderconfirmed" type="button" class="btn btn-success btn-lg">CONFIRM</a>
+                        <a
+                         onClick = {() => {setDiscount("0%");
+                         console.log(discount)
+                        console.log(number);
+                        console.log(address);
+                        console.log(area);
+                        console.log(floor); 
+                        console.log(deliverytype)}}
+                            // href = "/orderconfirmed"
+                             type="button" class="btn btn-success btn-lg">CONFIRM</a>
                     </div>
                     <br/>
                 </div>
