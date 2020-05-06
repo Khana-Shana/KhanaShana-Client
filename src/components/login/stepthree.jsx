026634c54db5beac7f42bf94f1f580dad558c1c5
mehreemas <mehreen@DESKTOP-486H0IP.localdomain>
2020-05-07 01:41:31 +0500

    cart-code-cleaning

--- src/components/login/stepthree.jsx
+++ src/components/login/stepthree.jsx
@@ -8,13 +8,6 @@ const StepThree = (props) => {
     <div className="logcardback2">
       <div className="sign-text">WELCOME!</div>
       <div className="prog1">
-        {/* <Line
-          percent="100"
-          strokeWidth="2"
-          strokeColor="#B74852"
-          trailWidth="1"
-          trailColor="white"
-        /> */}
         <img src ="https://firebasestorage.googleapis.com/v0/b/khana-shana-2020.appspot.com/o/Mehreen%2Fstep3.svg?alt=media&token=1c60ba44-3484-4aad-952f-c3b8c4311d79"/>
       </div>
       <div>
