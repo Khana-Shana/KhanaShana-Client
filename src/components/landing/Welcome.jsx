85870b000be6144065ae3d582382358428485bd2
mehreemas <mehreen@DESKTOP-486H0IP.localdomain>
2020-05-08 04:21:39 +0500

    landing

--- src/components/landing/Welcome.jsx
+++ src/components/landing/Welcome.jsx
@@ -40,7 +40,7 @@ function Welcome() {
                   id="GFG"
                   type="button"
                   className="button-error pure-button"
-                  style={{ fontSize: "1.7rem", width: "40%", border: "none", fontFamily: "'Jost', sans-serif", marginTop: "3%", marginLeft: "1%"}}
+                  style={{ fontSize: "1.7rem", width: "40%", border: "none", fontFamily: "'Jost', sans-serif", marginTop: "3%", marginLeft: "15%"}}
                 >
                   PLACE ORDER
               </button>
