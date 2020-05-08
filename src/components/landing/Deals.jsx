6fe944ac5ac0c8f7f6880fc4f1a9f7fc8ce8e943
mehreemas <mehreen@DESKTOP-486H0IP.localdomain>
2020-05-08 08:25:36 +0500

    menu

--- src/components/landing/Deals.jsx
+++ src/components/landing/Deals.jsx
@@ -1,19 +1,15 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import Wheel from "./Wheel";
+import { withRouter, Link } from "react-router-dom";
 import firebase_integration from "../fire.js";
-import DailyDealContext from "../context/dailydealcontext";
+import firebase from "../fire";
 import "./deals.css";
 
-function Deals() {
-  /* access context to set deal for menu */
-  const { setDaily } = useContext(DailyDealContext);
-  const [button, setbutton] = React.useState(false);
-
+function Deals(props) {
   /* states for reading deal details from database */
   const [dailydeal, setdailydeal] = React.useState({});
   const [weeklydeal, setweeklydeal] = React.useState({});
   const [discountwheel, setdiscountwheel] = React.useState([]);
+
   const [discountval, setDiscountval] = React.useState(0);
   const [discavail, setdiscavail] = React.useState(false);
 
@@ -52,76 +48,107 @@ function Deals() {
     weeklydeal,
     discountwheel
   );
-  let value = 0;
-  let discount = 0;
-  function selectItem() {
-    value = Math.floor(Math.random() * discountwheel.length);
-    setDiscountval(discountwheel[value]);
-    setdiscavail(true);
 
+  let value = 0;
+  async function selectItem(UserID) {
+    value = Math.floor(Math.random() * discountwheel.length);
+    console.log(value);
+    setDiscountval(discountwheel[value]);
+    console.log(discountval);
+    console.log(discountwheel[value]);
+    setdiscavail(true);
+    var todaysDate = new Date();
+    var disc = parseInt(
+      discountwheel[value].substring(0, discountwheel[value].length - 1)
+    );
+    console.log(disc);
+
+    firebase_integration.database
+      .collection("CustomerDatabase")
+      .doc(UserID.toString())
+      .update({
+        WheelUsed: true,
+        DateWheelUsed: todaysDate,
+        Discount: disc,
+      });
   }
 
   return (
-    <div style = {{width:"97.1%"}} className=" deals d-flex justify-content-center container-fluid">
+    <div
+      style={{ width: "97.1%" }}
+      className=" deals d-flex justify-content-center container-fluid"
+    >
       <div className="row pb-5">
         <div className="col-lg-5 col-sm wheelitem">
           <div className="wheeltitle">TRY YOUR LUCK</div>
-
           <div>
+            {discavail ? (
+              <div className="disc dealcard rounded">
+                <div class="discounts1">
+                  <div class="entry1" style={{ textAlign: "center" }}>
+                    {discountval}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="disc dealcard rounded">
+                <div class="discounts">
+                  <div class="entry" style={{ textAlign: "center" }}>
+                    {discountwheel[0]}
+                  </div>
+                  <div class="entry" style={{ textAlign: "center" }}>
+                    {discountwheel[1]}
+                  </div>
+                  <div class="entry" style={{ textAlign: "center" }}>
+                    {discountwheel[2]}
+                  </div>
+                </div>
+              </div>
+            )}
 
-{discavail ?
-  <div className = "disc dealcard rounded">
-<div class = "discounts1">
-  <div class = "entry1" style = {{textAlign:"center"}}>{discountval}</div>
-</div>
-</div>
-:
-<div className = "disc dealcard rounded">
-<div class = "discounts">
-  <div class = "entry" style = {{textAlign:"center"}}>{discountwheel[0]}</div>
-  <div class = "entry" style = {{textAlign:"center"}}>{discountwheel[1]}</div>
-  <div class = "entry" style = {{textAlign:"center"}}>{discountwheel[2]}</div>
-</div>
-</div>
-}
- 
             <div>
-              {discavail ? (
-                // <Link to="/fullmenu">
-                  <button
-                    id="GFG"
-                    type="button"
-                    className="button-error pure-button"
-                    style={{
-                      fontSize: "1.7rem",
-                      width: "50%",
-                      border: "none",
-                      fontFamily: "'Jost', sans-serif",
-                      marginTop: "8%",
-                      marginLeft: "25%",
-                    }}
-                  disabled>
-                    Avail Discount
-                  </button>
-                // </Link>
-              ) : (
-                <button
-                  id="GFG"
-                  onClick = {selectItem}
-                  type="button"
-                  className="button-error pure-button"
-                  style={{
-                    fontSize: "1.7rem",
-                    width: "50%",
-                    border: "none",
-                    fontFamily: "'Jost', sans-serif",
-                    marginTop: "8%",
-                    marginLeft: "25%",
-                  }}
-                >
-                  Avail Discount
-                </button>
-              )}
+              <button
+                id="GFG"
+                onClick={() => {
+                  if (!firebase.getCurrentUsername()) {
+                    alert("Please login first");
+                    props.history.replace("/loginpage");
+                    return null;
+                  } else {
+                    var UserID = firebase_integration.auth.currentUser.uid;
+                    firebase_integration.database
+                      .collection("CustomerDatabase")
+                      .where("CustomerID", "==", UserID.toString())
+                      .get()
+                      .then((docs) => {
+                        var mydata = 0;
+                        docs.forEach((doc) => {
+                          mydata = doc.data();
+                        });
+                        if (mydata.WheelUsed === false) {
+                          selectItem(UserID);
+                        } else if (mydata.WheelUsed === true) {
+                          setDiscountval("0%");
+                          alert(
+                            "Discount already availed. Try again next week!"
+                          );
+                        }
+                      });
+                  }
+                }}
+                type="button"
+                className="button-error pure-button"
+                style={{
+                  fontSize: "1.7rem",
+                  width: "50%",
+                  border: "none",
+                  fontFamily: "'Jost', sans-serif",
+                  marginTop: "8%",
+                  marginLeft: "25%",
+                }}
+              >
+                Avail Discount
+              </button>
             </div>
           </div>
         </div>
@@ -190,4 +217,4 @@ function Deals() {
   );
 }
 
-export default Deals;
+export default withRouter(Deals);
